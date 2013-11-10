c54a7ec411fc95fa025835f5aff3cb5521f3bc43
Joe Bruner <jbruner@42six.com>
2013-11-10 10:19:26 -0500

    Renamed the ReverseHypothesis* classes to ReverseFeatureLookup*.  Got rid of the reducer and changed it to use the IdentityReducer instead.

---
+++ amino-impl/database/accumulo/common/src/main/java/com/_42six/amino/bitmap/reverse/ReverseFeatureLookupJob.java
@@ -0,0 +1,169 @@
+package com._42six.amino.bitmap.reverse;
+
+import com._42six.amino.bitmap.BitmapConfigHelper;
+import com._42six.amino.common.AminoConfiguration;
+import com._42six.amino.common.JobUtilities;
+import com._42six.amino.common.accumulo.IteratorUtils;
+import com._42six.amino.common.util.PathUtils;
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.accumulo.core.client.Instance;
+import org.apache.accumulo.core.client.ZooKeeperInstance;
+import org.apache.accumulo.core.client.impl.ConnectorImpl;
+import org.apache.accumulo.core.client.mapreduce.AccumuloFileOutputFormat;
+import org.apache.accumulo.core.client.mapreduce.lib.partition.KeyRangePartitioner;
+import org.apache.accumulo.core.client.mapreduce.lib.partition.RangePartitioner;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.util.TextUtil;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.BufferedOutputStream;
+import java.io.PrintStream;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ReverseFeatureLookupJob extends Configured implements Tool
+{
+    private static final String BIGTABLE_INSTANCE = "bigtable.instance";
+    private static final String BIGTABLE_ZOOKEEPERS = "bigtable.zookeepers";
+    private static final String BIGTABLE_USERNAME = "bigtable.username";
+    private static final String BIGTABLE_PASSWORD = "bigtable.password";
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.set(AminoConfiguration.DEFAULT_CONFIGURATION_PATH_KEY, args[1]); // TODO: use flag instead of positional
+        AminoConfiguration.loadDefault(conf, "AminoDefaults", true);
+
+        int res = ToolRunner.run(conf, new ReverseFeatureLookupJob(), args);
+        System.exit(res);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception
+    {
+        System.out.println("\n================================ ReverseFeatureLookupJob ================================\n");
+        final Configuration conf = getConf();
+
+        final Job job = new Job(conf, "Amino reverse_feature_lookup table job");
+        job.setJarByClass(ReverseFeatureLookupJob.class);
+        job.setInputFormatClass(SequenceFileInputFormat.class);
+
+        // Parse the command line parameters
+        final String inputPaths = StringUtils.join(PathUtils.getJobDataPaths(conf, args[0]), ',');
+        final String cachePaths = StringUtils.join(PathUtils.getJobCachePaths(conf, args[0]), ',');
+        final String workingDirectory = args[2]; // TODO: use configuration instead of positional argument
+
+        System.out.println("Input paths: [" + inputPaths + "].");
+        System.out.println("Cache paths: [" + cachePaths + "].");
+
+        JobUtilities.deleteDirectory(conf, workingDirectory);
+        PathUtils.setCachePath(job.getConfiguration(), cachePaths);
+        SequenceFileInputFormat.setInputPaths(job, inputPaths);
+
+        // Configure the mapper
+        job.setMapperClass(ReverseFeatureLookupMapper.class);
+        job.setMapOutputKeyClass(Key.class);
+        job.setMapOutputValueClass(Value.class);
+
+        // Configure the reducer
+        // Use the IdentityReducer - no need to call job.setReducerClass()
+        job.setOutputKeyClass(Key.class);
+        job.setOutputValueClass(Value.class);
+
+        final int numberOfShards = conf.getInt(BitmapConfigHelper.BITMAP_CONFIG_NUM_SHARDS, 10);
+        job.setNumReduceTasks(numberOfShards);
+
+        // Grab params for connecting to BigTable instance
+        final String instanceName = conf.get(BIGTABLE_INSTANCE);
+        final String zooKeepers = conf.get(BIGTABLE_ZOOKEEPERS);
+        final String user = conf.get(BIGTABLE_USERNAME);
+        final String password = conf.get(BIGTABLE_PASSWORD);
+        final String tableName = conf.get("amino.bitmap.featureLookupTable").replace("amino_", "amino_reverse_");
+
+        boolean blastIndex = conf.getBoolean("amino.bitmap.first.run", true); // should always assume it's the first run unless specified
+
+        ConnectorImpl connector = null;
+        PrintStream splitsPrinter = null;
+
+        boolean success = false;
+        final FileSystem fs = FileSystem.get(conf);
+
+        // Create the splits for the splitfile
+        final SortedSet<Text> splits = new TreeSet<Text>();
+        for (int shard = 0; shard < numberOfShards; shard++)
+        {
+            splits.add(new Text(Integer.toString(shard)));
+        }
+
+        try
+        {
+            final Instance instance = new ZooKeeperInstance(instanceName, zooKeepers);
+            connector = new ConnectorImpl(instance, user, password.getBytes());
+
+            splitsPrinter = new PrintStream(new BufferedOutputStream(fs.create(new Path(workingDirectory + "/splits.txt"))));
+            for (Text split : splits)
+            {
+                splitsPrinter.println(new String(Base64.encodeBase64(TextUtil.getBytes(split))));
+            }
+            splitsPrinter.flush();
+            splitsPrinter.close();
+
+            success = IteratorUtils.createTable(connector.tableOperations(), tableName, splits, blastIndex, blastIndex);
+
+            job.setOutputFormatClass(AccumuloFileOutputFormat.class);
+            AccumuloFileOutputFormat.setOutputPath(job, new Path(workingDirectory + "/files"));
+            job.setPartitionerClass(KeyRangePartitioner.class);
+            RangePartitioner.setSplitFile(job, workingDirectory + "/splits.txt");
+        }
+        catch (AccumuloException e)
+        {
+            e.printStackTrace();
+        }
+        catch (AccumuloSecurityException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            if(splitsPrinter != null){
+                splitsPrinter.close();
+            }
+        }
+
+        int result = 0;
+        if (success)
+        {
+            result = job.waitForCompletion(true) ? 0 : -1;
+        }
+
+        // Bulk import the results into the database
+        if (connector != null && success)
+        {
+            System.out.println("Importing job results to accumulo");
+            try
+            {
+                final String importTable = (!blastIndex) ? tableName : tableName + IteratorUtils.TEMP_SUFFIX;
+                connector.tableOperations().importDirectory(importTable, workingDirectory + "/files", workingDirectory + "/failures", 20, 4, false);
+                result = JobUtilities.failureDirHasFiles(conf, workingDirectory + "/failures");
+            }
+            catch (Exception e)
+            {
+                result = 1;
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
+
+}
--- amino-impl/database/accumulo/common/src/main/java/com/_42six/amino/bitmap/reverse/ReverseFeatureLookupMapper.java
+++ amino-impl/database/accumulo/common/src/main/java/com/_42six/amino/bitmap/reverse/ReverseFeatureLookupMapper.java
@@ -6,44 +6,35 @@ import com._42six.amino.common.Bucket;
 import com._42six.amino.common.BucketStripped;
 import com._42six.amino.common.index.BitmapIndex;
 import com._42six.amino.common.service.datacache.BucketCache;
-import com._42six.amino.common.service.datacache.BucketNameCache;
-import com._42six.amino.common.service.datacache.DataSourceCache;
-import com._42six.amino.common.service.datacache.VisibilityCache;
 import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Value;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class ReverseHypothesisMapper extends Mapper<BucketStripped, AminoWritable, Text, ReverseHypothesisValue>
+public class ReverseFeatureLookupMapper extends Mapper<BucketStripped, AminoWritable, Key, Value>
 {
-	private BucketCache bucketCache;
-    private BucketNameCache bucketNameCache;
-    private DataSourceCache dataSourceCache;
-    private VisibilityCache visibilityCache;
+    private BucketCache bucketCache;
     private BucketStripped lastBS;
 
     private int numberOfShards;
     private int numberOfHashes;
 
     @Override
-	protected void setup(Context context) throws IOException, InterruptedException {
-    	super.setup(context);
+    protected void setup(Context context) throws IOException, InterruptedException {
+        super.setup(context);
         final Configuration conf = context.getConfiguration();
-    	bucketCache = new BucketCache(conf);
-        bucketNameCache = new BucketNameCache(conf);
-        dataSourceCache = new DataSourceCache(conf);
-        visibilityCache = new VisibilityCache(conf);
+        bucketCache = new BucketCache(conf);
 
-		numberOfShards = context.getConfiguration().getInt(BitmapConfigHelper.BITMAP_CONFIG_NUM_SHARDS, 10);
-	    numberOfHashes = context.getConfiguration().getInt("amino.bitmap.num-hashes", 1);
+        numberOfShards = context.getConfiguration().getInt(BitmapConfigHelper.BITMAP_CONFIG_NUM_SHARDS, 10);
+        numberOfHashes = context.getConfiguration().getInt("amino.bitmap.num-hashes", 1);
     }
 
-	@Override
-	protected void map(BucketStripped bs, AminoWritable aw, Context context) throws IOException, InterruptedException
-	{
+    @Override
+    protected void map(BucketStripped bs, AminoWritable aw, Context context) throws IOException, InterruptedException
+    {
         // Note: We currently don't care what the aw value is as we don't need it for anything
 
         /**
@@ -62,26 +53,19 @@ public class ReverseHypothesisMapper extends Mapper<BucketStripped, AminoWritabl
         }
 
         final Bucket bucket = bucketCache.getBucket(bs);
-        final IntWritable BUCKETNAME = bucketNameCache.getIndexForValue(bucket.getBucketName());
-        final IntWritable DATASOURCE = dataSourceCache.getIndexForValue(bucket.getBucketDataSource());
-        final IntWritable VISIBILITY = visibilityCache.getIndexForValue(bucket.getBucketVisibility());
-        final String BUCKET_VALUE = bucket.getBucketValue().toString();
+        final Value value = new Value("".getBytes());
 
         // Make sure that we have the same shard for all of the salts
         int index = BitmapIndex.getValueIndex(bucket, 0);
         final Text SHARD = new Text(Integer.toString(index % numberOfShards));
 
-        final ReverseHypothesisValue rhv = new ReverseHypothesisValue(index, DATASOURCE, BUCKETNAME, 0, BUCKET_VALUE, VISIBILITY);
-
         // Write the Key/Value for each salt
         for (int salt = 0; salt < numberOfHashes; salt++)
         {
-        	index = BitmapIndex.getValueIndex(bucket, salt);
+            index = BitmapIndex.getValueIndex(bucket, salt);
             final Key cbKey = new Key(SHARD, new Text(Integer.toString(index) + "#" + bucket.getBucketDataSource() + "#" + bucket.getBucketName() + "#" + salt),
                     bucket.getBucketValue(), bucket.getBucketVisibility());
-        	rhv.setSalt(salt);
-            rhv.setIndexPos(index);
-            context.write(new Text(cbKey.toStringNoTime()), rhv);
+            context.write(cbKey, value);
         }
-	}
+    }
 }
