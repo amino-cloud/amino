f407de4528d0c4e27c23d232d0329183699e70cb
James Stapleton <github@tasermonkeys.com>
2014-06-06 10:04:56 -0400

    Reformatting code, since it used 2 different formatting schemes.

--- amino-core/amino-common/src/main/java/com/_42six/amino/common/translator/FeatureFactTranslatorImpl.java
+++ amino-core/amino-common/src/main/java/com/_42six/amino/common/translator/FeatureFactTranslatorImpl.java
@@ -7,56 +7,56 @@ import java.math.BigDecimal;
 
 /**
  * This implementation converts double ratios and intervals to text by:
- * 	  - adding a large constant value to the ratio/interval
- * 	  - padding the ratio/interval with zeroes on the beginning and end
+ *   - adding a large constant value to the ratio/interval
+ *   - padding the ratio/interval with zeroes on the beginning and end
  * It converts them back to double by:
- *    - subtracting the same large constant value from the ratio/interval
- * 
+ *   - subtracting the same large constant value from the ratio/interval
+ * <p/>
  * This ensures both positive and negative interval/ratios are correctly
  * ascii sorted when scanned.
  */
 public class FeatureFactTranslatorImpl implements FeatureFactTranslatorInt {
-	
-	public static final int CONVERT_RATIO_CONSTANT 	  = 100000000;
-	public static final int CONVERT_INTERVAL_CONSTANT = 100000000;
-	public static final BigDecimal CONVERT_RATIO_CONSTANT_BD = new BigDecimal(CONVERT_RATIO_CONSTANT);
-	public static final BigDecimal CONVERT_INTERVAL_CONSTANT_BD = new BigDecimal(CONVERT_INTERVAL_CONSTANT);
-	
-	public Text fromFeatureFact(FeatureFact fact) {
-		return new Text(fact.getFact().toString());
-	}
 
-	@Override
-	public Text fromRatio(double fact) {
-		if(fact == Double.MAX_VALUE){
-			return new Text("max");
-		} else if(fact == Double.MIN_VALUE){
-			return new Text("min");
-		} else{
-			return new Text(String.format("%018.8f", fact + CONVERT_RATIO_CONSTANT));
-		}
-	}
-	
-	@Override
-	public double toRatio(String fact) {
-		if(fact.compareToIgnoreCase("max") == 0){
-			return Double.MAX_VALUE;
-		} else if(fact.compareToIgnoreCase("min") == 0){
-			return Double.MIN_VALUE;
-		} else {	
-			return (new BigDecimal(fact)).subtract(CONVERT_RATIO_CONSTANT_BD).doubleValue();
-		}
-	}
+    public static final int CONVERT_RATIO_CONSTANT    = 100000000;
+    public static final int CONVERT_INTERVAL_CONSTANT = 100000000;
+    public static final BigDecimal CONVERT_RATIO_CONSTANT_BD = new BigDecimal(CONVERT_RATIO_CONSTANT);
+    public static final BigDecimal CONVERT_INTERVAL_CONSTANT_BD = new BigDecimal(CONVERT_INTERVAL_CONSTANT);
 
-	@Override
-	public Text fromInterval(double fact) {
-		return new Text(String.format("%018.8f", fact + CONVERT_INTERVAL_CONSTANT));
-	}
+    public Text fromFeatureFact(FeatureFact fact) {
+        return new Text(fact.getFact().toString());
+    }
 
-	@Override
-	public double toInterval(String fact) {
-		return (new BigDecimal(fact)).subtract(CONVERT_INTERVAL_CONSTANT_BD).doubleValue();
-	}
+    @Override
+    public Text fromRatio(double fact) {
+        if (fact == Double.MAX_VALUE) {
+            return new Text("max");
+        } else if (fact == Double.MIN_VALUE) {
+            return new Text("min");
+        } else {
+            return new Text(String.format("%018.8f", fact + CONVERT_RATIO_CONSTANT));
+        }
+    }
+
+    @Override
+    public double toRatio(String fact) {
+        if (fact.compareToIgnoreCase("max") == 0) {
+            return Double.MAX_VALUE;
+        } else if (fact.compareToIgnoreCase("min") == 0) {
+            return Double.MIN_VALUE;
+        } else {
+            return (new BigDecimal(fact)).subtract(CONVERT_RATIO_CONSTANT_BD).doubleValue();
+        }
+    }
+
+    @Override
+    public Text fromInterval(double fact) {
+        return new Text(String.format("%018.8f", fact + CONVERT_INTERVAL_CONSTANT));
+    }
+
+    @Override
+    public double toInterval(String fact) {
+        return (new BigDecimal(fact)).subtract(CONVERT_INTERVAL_CONSTANT_BD).doubleValue();
+    }
 
     // since timestamps are never negative and specified as a whole number of milliseconds, simply
     // store longs as left justified zero padded hex.
